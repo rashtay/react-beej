7fb6874379c6f059479281a1ce859e6d713564bb
Rahul Shetty <rahul.shetty@here.com>
2019-08-11 20:50:30 +0530

    Fix: Redux persist flow issues

--- flow-typed/libdefs.js
+++ flow-typed/libdefs.js
@@ -1,6 +1,10 @@
 // @flow
 declare var module: Object;
 
-declare module 'redux-persist/' {
+declare module 'redux-persist' {
+  declare module.exports: any;
+}
+
+declare module 'redux-persist/lib' {
   declare module.exports: any;
 }
